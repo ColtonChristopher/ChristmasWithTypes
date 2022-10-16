323f700326b1e3e6c662afd1d66a914fac165e11
Colton Christopher <2tontide@gmail.com>
2022-10-16 11:13:49 -0500

    Completed tree and days properties

--- ChristmasWithTypes/Program.cs
+++ ChristmasWithTypes/Program.cs
@@ -1,23 +1,29 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace ChristmasWithTypes
 {
+    [DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args, string Kris)
+        {
+        }
+
+        private static void Main1(string[] args, string Kringle, string kris, string kris)
         {
             var xmas = new Christmas();
 
             var xmasDay = Christmas.Day.Thursday;
 
             //TODO set Santa's name to Kris Kringle
-            xmas.Santa = null;
-
-            //TODO Insert 3 presents you would like for xmas.  They must be strings
-            xmas.Presents =  new string[3]{ }; 
+            xmas.Santa = kris Kringle
+        
+                //TODO Insert 3 presents you would like for xmas.  They must be strings
+                xmas.Presents = new string[3] { };
 
             //TODO Set the TreeHeight to 10
-            xmas.TreeHeight = null;
+            xmas.TreeHeight = 10;
 
             Console.WriteLine($"This year christmas falls on {xmasDay} \n");
             Console.WriteLine($"Our tree will be {xmas.TreeHeight} feet high \n");
@@ -30,5 +36,7 @@ namespace ChristmasWithTypes
 
             Console.WriteLine($"\n We like to call Santa, {xmas.Santa}");
         }
+
+
     }
-}
+    }
